9bad9b4d347206e0499e1531f518ac373dd46a32
mahsafateh <mahsafaateh@gmail.com>
2025-08-05 14:07:22 +0200

    refactor: setting screens seprate components (#8)

--- screens/settingScreens/SoftwareUpdatesScreen.tsx
+++ screens/settingScreens/SoftwareUpdatesScreen.tsx
@@ -1,8 +1,7 @@
 import { View, Text } from "react-native";
-import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const SoftwareUpdates = () => {
+const SoftwareUpdatesScreen = () => {
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center items-center">
@@ -13,4 +12,4 @@ const SoftwareUpdates = () => {
   );
 };
 
-export default SoftwareUpdates;
+export default SoftwareUpdatesScreen;
