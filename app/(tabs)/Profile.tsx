1c913f962bb7ad351c5b5c2292e01c1b4f1d6bd2
mahsafateh <mahsafaateh@gmail.com>
2025-08-05 14:44:42 +0200

    refactor: minor styling change Profile(#17)

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -3,9 +3,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 function Profile() {
   return (
-    <SafeAreaView className={"flex-1"}>
-      <View className="flex-1">
-        <Text>Profile</Text>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-lg font-medium">User Spasider</Text>
+        <Text className="text-gray-600 mt-2">Version 1.0.0</Text>
       </View>
     </SafeAreaView>
   );
--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,7 +1,6 @@
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SettingTile } from "@/components/SettingTile";
-import Title from "@/components/Title";
 
 const Index = ({ navigation }: any) => {
   return (
