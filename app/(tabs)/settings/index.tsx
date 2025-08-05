32fe5bbe362312f977a394c2f60e5ecfc67ccb2d
mahsafateh <mahsafaateh@gmail.com>
2025-08-05 14:27:19 +0200

    refactor: minor styling change(#15)

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,12 +1,13 @@
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SettingTile } from "@/components/SettingTile";
+import Title from "@/components/Title";
 
 const Index = ({ navigation }: any) => {
   return (
     <SafeAreaView className={"flex-1"}>
-      <ScrollView className="flex-1">
-        <View className={"pt-4 px-2"}>
+      <ScrollView className="flex-1 px-4 pt-4">
+        <View className={"px-2 bg-white rounded-xl"}>
           <SettingTile
             icon="language"
             label="Language"
