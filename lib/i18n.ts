5c45b2caeeabf85fd7215679bbe21ffff014ff63
Mahsa Fateh <mahsafaateh@gmail.com>
2025-09-01 15:33:23 +0200

    ft: initiated in root

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,29 +1,41 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import { NativeModules, Platform } from "react-native";
 
 import en from "@/locales/en/translation.json";
 import fa from "@/locales/fa/translation.json";
 import sv from "@/locales/sv/translation.json";
 
-i18n
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .init({
-    lng: undefined,
-    fallbackLng: "en",
-    resources: {
-      en: { translation: en },
-      fa: { translation: fa },
-      sv: { translation: sv },
-    },
-    supportedLngs: ["en", "fa", "sv"],
-    interpolation: {
-      escapeValue: false,
-    },
-    // detection: {
-    //   order: ["localStorage", "navigator"],
-    // }
-  });
+// Get device language for React Native
+const getDeviceLanguage = () => {
+  if (Platform.OS === "ios") {
+    return (
+      NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0]
+    );
+  } else {
+    return NativeModules.I18nManager.localeIdentifier;
+  }
+};
+
+const deviceLanguage = getDeviceLanguage();
+const languageCode = deviceLanguage?.split(/[-_]/)[0] || "en";
+
+i18n.use(initReactI18next).init({
+  lng: languageCode,
+  fallbackLng: "en",
+  resources: {
+    en: { translation: en },
+    fa: { translation: fa },
+    sv: { translation: sv },
+  },
+  supportedLngs: ["en", "fa", "sv"],
+  interpolation: {
+    escapeValue: false,
+  },
+  react: {
+    useSuspense: false,
+  },
+});
 
 export default i18n;
