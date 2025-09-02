0b81d36bef1ff2f8b14fb9f17b40660541949f40
Mahsa Fateh <mahsafaateh@gmail.com>
2025-09-02 10:34:45 +0200

    wip: preview

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,28 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { NativeModules, Platform } from "react-native";
 
 import en from "@/locales/en/translation.json";
 import fa from "@/locales/fa/translation.json";
 import sv from "@/locales/sv/translation.json";
 
-// Get device language for React Native
-const getDeviceLanguage = () => {
-  if (Platform.OS === "ios") {
-    return (
-      NativeModules.SettingsManager.settings.AppleLocale ||
-      NativeModules.SettingsManager.settings.AppleLanguages[0]
-    );
-  } else {
-    return NativeModules.I18nManager.localeIdentifier;
-  }
-};
-
-const deviceLanguage = getDeviceLanguage();
-const languageCode = deviceLanguage?.split(/[-_]/)[0] || "en";
-
 i18n.use(initReactI18next).init({
-  lng: languageCode,
+  lng: "en",
   fallbackLng: "en",
   resources: {
     en: { translation: en },
--- screens/settingScreens/LanguageScreen.tsx
+++ screens/settingScreens/LanguageScreen.tsx
@@ -1,14 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const LanguageScreen = () => {
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center items-center">
-        <Text className="text-lg font-medium">Language Settings</Text>
-        <Text className="text-gray-600 mt-2">
+        <Text className="text-gray-600 text-lg font-medium">
           Choose your preferred language
         </Text>
+
+        <Button title="English" />
+        <Button title="Persian" />
+        <Button title="Svenska" />
       </View>
     </SafeAreaView>
   );
