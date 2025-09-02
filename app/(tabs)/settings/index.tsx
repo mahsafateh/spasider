25911ca017324b2ac0c1521f6bf154be6da48fc3
Mahsa Fateh <mahsafaateh@gmail.com>
2025-09-02 16:04:22 +0200

    wip: refactor on custom tile component

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,37 +1,39 @@
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { SettingTile } from "@/components/SettingTile";
+import { Tile } from "@/components/Tile";
 
 import i18next from "i18next";
 
-const Index = ({ navigation }: any) => {
-  return (
-    <SafeAreaView className={"flex-1"}>
-      <ScrollView className="flex-1 px-4 pt-4">
-        <View className={"px-2 bg-white rounded-xl"}>
-          <SettingTile
-            icon="language"
-            label={i18next.t("settings.language")}
-            onPress={() => navigation.navigate("Language")}
-          />
-          <SettingTile
-            icon="color-palette-outline"
-            label={i18next.t("settings.theme")}
-            onPress={() => navigation.navigate("Theme")}
-          />
-          <SettingTile
-            icon="information-circle-outline"
-            label={i18next.t("settings.about")}
-            onPress={() => navigation.navigate("About")}
-          />
-          <SettingTile
-            icon="cloud-download-outline"
-            label={i18next.t("settings.softwareUpdates")}
-            onPress={() => navigation.navigate("SoftwareUpdates")}
-          />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+const Index = ({ navigation }: any) => (
+  <SafeAreaView className={"flex-1"}>
+    <ScrollView className="flex-1 px-4 pt-4">
+      <View className={"px-2 bg-white rounded-xl"}>
+        <Tile
+          icon="language"
+          label={i18next.t("settings.language")}
+          onPress={() => navigation.navigate("Language")}
+          visible={true}
+        />
+        <Tile
+          icon="color-palette-outline"
+          label={i18next.t("settings.theme")}
+          onPress={() => navigation.navigate("Theme")}
+          visible={true}
+        />
+        <Tile
+          icon="information-circle-outline"
+          label={i18next.t("settings.about")}
+          onPress={() => navigation.navigate("About")}
+          visible={true}
+        />
+        <Tile
+          icon="cloud-download-outline"
+          label={i18next.t("settings.softwareUpdates")}
+          onPress={() => navigation.navigate("SoftwareUpdates")}
+          visible={true}
+        />
+      </View>
+    </ScrollView>
+  </SafeAreaView>
+);
 export default Index;
--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,12 +1,14 @@
 import { Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
-export const SettingTile = ({ icon, label, onPress }: any) => {
+export const Tile = ({ icon, label, onPress, visible }: any) => {
   return (
     <TouchableOpacity className="flex-row items-center p-4 " onPress={onPress}>
-      <Icon name={icon} size={24} color="#888" className={"mr-4"} />
+      {visible ? (
+        <Icon name={icon} size={24} color="#888" className={"mr-4"} />
+      ) : null}
       <Text className="flex-1 text-sm font-quicksand-semibold">{label}</Text>
-      <Icon name="chevron-forward" size={20} color="#aaa" />
+      {visible ? <Icon name="chevron-forward" size={20} color="#aaa" /> : null}
     </TouchableOpacity>
   );
 };
